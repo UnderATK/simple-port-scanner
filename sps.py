c7370dea11735636b77b59d07a67755c49ef6cae
Dima Shapira <33936831+UnderATK@users.noreply.github.com>
2024-08-13 10:42:41 -0400

    Update sps.py
    v 0.2.0

--- sps.py
+++ sps.py
@@ -1,19 +1,27 @@
+#!/bin/python3
 import socket
 from datetime import datetime
+import sys
 
 # define enums
-VR = "0.1.0"
+VR = "0.2.0"
 AUTHOR = "UnderATK"
-ERR = "\033[31m[Error]"
+# text coloring enums
+GREY_TXT = "\033[30m"
+ERR_TXT = "\033[31m[Error]"
+
+# define variables
+###
 
 def scanner(target):
     try:
         targetIP = socket.gethostbyname(target)
 
-        print("----------------------------------")
+        print("-" * 75)
         print(f"Scanning the target: {target} | {targetIP}")
         print(f"Scan starting time: {datetime.now()}")
-        print("----------------------------------")
+        print("-" * 75)
+
         for port in range(1,1025): 
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(1)
@@ -28,13 +36,30 @@ def scanner(target):
             sock.close()
 
     except socket.gaierror:
-        print(f"{ERR} Hostname could not be resolved.")
+        print(f"{ERR_TXT} Hostname could not be resolved.")
+        sys.exit()
 
     except socket.error:
-        print(f"{ERR} could not connet to the server.")
+        print(f"{ERR_TXT} could not connet to the server.")
+        sys.exit()
 
-print(f"----- Simple Port Scanner v{VR} -----")
-print(f"----- By {AUTHOR}                -----")
-target = input("Enter the target IP address or domain: ")
-scanner(target)
+    except KeyboardInterrupt:
+        print("\nExiting sps.py")
+        sys.exit()
+
+# Welcome banner
+print("-" * 50)
+print(f"-----\tSimple Port Scanner v{VR}")
+print(f"-----\tBy {AUTHOR}")
+print("-" * 50)
+
+# Checking correct amount of arguments
+if len(sys.argv) == 2:
+    target = sys.argv[1]
+    scanner(target)
+else: 
+    print(f"{ERR_TXT}Invalid amount of arguments.")
+    print(f"{GREY_TXT}Syntax: python3 sps.py <ip/domain>")
+    sys.exit()
+
 
